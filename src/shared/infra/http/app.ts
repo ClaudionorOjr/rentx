c997f7aaaa1c0f3a5861e428de274eb7bc7bf03d
Claudionor <claudionorojr@hotmail.com>
2022-10-25 16:04:07 -0300

    Cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -4,6 +4,7 @@ import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors"
 
 import swaggerUi from 'swagger-ui-express'
+import cors from "cors"
 
 import swaggerFile from "../../../swagger.json"
 import { AppError } from "@shared/errors/AppError";  
@@ -24,6 +25,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use("/avatar", express.static(`${upload.tpmFolder}/avatar`))
 app.use("/cars", express.static(`${upload.tpmFolder}/cars`))
 
+app.use(cors())
 app.use(router)
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
